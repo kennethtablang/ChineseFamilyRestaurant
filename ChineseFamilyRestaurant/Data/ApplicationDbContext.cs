2b19962c251f27a6962b29423fee547552338c25
kennethtablang <kennethreytablang@gmail.com>
2024-11-19 15:35:36 +0800

    Add project files.

---
+++ ChineseFamilyRestaurant/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ChineseFamilyRestaurant.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChineseFamilyRestaurant.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ ChineseFamilyRestaurant/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace ChineseFamilyRestaurant.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; } // a product belongs to another category
+        public ICollection<OrderItem> OrderItems { get; set; } //  a product can be in a many order items
+        public ICollection<ProductIngredient> ProductIngredient { get; set; } // a product can have many ingredients
+    }
+}
